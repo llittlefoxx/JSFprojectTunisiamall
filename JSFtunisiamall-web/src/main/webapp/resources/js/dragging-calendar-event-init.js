e0bc0a68f6c435d0a4ddab867a2d1114b5da3404
Elyes <elyes.benabdelkader@esprit.tn>
2016-05-02 19:39:09 +0100

    asma js update

--- JSFtunisiamall-web/src/main/webapp/resources/js/dragging-calendar-event-init.js
+++ JSFtunisiamall-web/src/main/webapp/resources/js/dragging-calendar-event-init.js
@@ -32,7 +32,11 @@ var Script = function () {
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
-
+    var my_string = "";
+    
+    my_string = $('#calender').val();
+    console.log(my_string);
+    var init = eval(my_string);
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -62,54 +66,8 @@ var Script = function () {
                 // if so, remove the element from the "Draggable Events" list
                 $(this).remove();
             }
-
         },
-        events: [
-            {
-                title: 'All Day Event',
-                start: new Date(y, m, 1)
-            },
-            {
-                title: 'Long Event',
-                start: new Date(y, m, d-5),
-                end: new Date(y, m, d-2)
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: new Date(y, m, d-3, 16, 0),
-                allDay: false
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: new Date(y, m, d+4, 16, 0),
-                allDay: false
-            },
-            {
-                title: 'Meeting',
-                start: new Date(y, m, d, 10, 30),
-                allDay: false
-            },
-            {
-                title: 'Lunch',
-                start: new Date(y, m, d, 12, 0),
-                end: new Date(y, m, d, 14, 0),
-                allDay: false
-            },
-            {
-                title: 'Birthday Party',
-                start: new Date(y, m, d+1, 19, 0),
-                end: new Date(y, m, d+1, 22, 30),
-                allDay: false
-            },
-            {
-                title: 'Click for Google',
-                start: new Date(y, m, 28),
-                end: new Date(y, m, 29),
-                url: 'http://google.com/'
-            }
-        ]
+        events: init
     });
 
 
