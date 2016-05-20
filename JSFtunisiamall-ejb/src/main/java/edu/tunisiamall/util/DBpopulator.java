fe59127f823ab10d784075b3840872835d614cee
Elyes <elyes.benabdelkader@esprit.tn>
2016-05-20 20:45:14 +0100

    zae

--- JSFtunisiamall-ejb/src/main/java/edu/tunisiamall/util/DBpopulator.java
+++ JSFtunisiamall-ejb/src/main/java/edu/tunisiamall/util/DBpopulator.java
@@ -9,43 +9,49 @@ import javax.ejb.Startup;
 
 import edu.tunisiamall.biservices.IndicatorsServiceRemote;
 import edu.tunisiamall.categorieServices.CategoryServicesRemote;
+import edu.tunisiamall.entities.Administrator;
 import edu.tunisiamall.entities.Category;
+import edu.tunisiamall.entities.Customer;
 import edu.tunisiamall.entities.Product;
 import edu.tunisiamall.entities.Promotion;
 import edu.tunisiamall.entities.Shopowner;
 import edu.tunisiamall.entities.Store;
 import edu.tunisiamall.entities.Subcategory;
 import edu.tunisiamall.entities.User;
+import edu.tunisiamall.userServices.userServicesLocal;
 import edu.tunisiamall.userServices.userServicesRemote;
-
 
 public class DBpopulator {
 
 	@EJB
 	IndicatorsServiceRemote indicatorsServiceRemote;
 	@EJB
-	CategoryServicesRemote categoryServicesRemote; 
+	CategoryServicesRemote categoryServicesRemote;
 	@EJB
 	userServicesRemote userServicesRemote;
-	
-	public DBpopulator(){
-		
+
+	public DBpopulator() {
+
 	}
-	
+
 	@PostConstruct
 	public void createData() {
 		Date date=new Date();
-		User user=new User("1 rue de liberte",date,"Dali","M","Eng","admin","admin","dali@dali.com","admin","22222222","http://localhost/pictures/user.png");
-		User user1=new User("2 rue de liberte",date,"Sami","M","Eng","user","user","sami@sami.com","user","22222221","http://localhost/pictures/user.png");
+		/*
+		Administrator admin = userServicesRemote.f
+		Customer c1 =  userServicesRemote.find(2);
+		Customer c2 =  userServicesRemote.find(3);
+		Shopowner s1 =  userServicesRemote.find(4);
+		Shopowner s2 =  userServicesRemote.find(5);
 		
 		
 		Category category=new Category("Cette categorie contient des vettement hiver","Winter Clothes");
 		Category category1=new Category("Cette categorie contient des produis alimentaire","Food");
 		Category category2=new Category("Cette categorie contient du matériel informatique","Computers");
 		
-		Store store=new Store("Store for fashion",category,"BERSHKA",date,"OPEN","33333333",(Shopowner)user1);
-		Store store1=new Store("Food store",category,"CARREFOUR",date,"OPEN","33333332",(Shopowner)user1);
-		Store store2=new Store("Technology store",category,"MEDIA-TEK",date,"OPEN","33333331",(Shopowner)user1);
+		Store store = new Store("Store for fashion",category,"BERSHKA",date,"OPEN","33333333",s1);
+		Store store1 = new Store("Food store",category,"CARREFOUR",date,"OPEN","33333332",s1);
+		Store store2 = new Store("Technology store",category,"MEDIA-TEK",date,"OPEN","33333331",s2);
 		
 		Subcategory subCategory=new Subcategory("this sub-category contains Shoes","Shoes",category);
 		Subcategory subCategory1=new Subcategory("this sub-category contains food","Cake",category1);
@@ -59,8 +65,11 @@ public class DBpopulator {
 		Product product2=new Product(25.0,date,"Biscuit Major",250,15,800,"Available","Food",18,null,null,store1,subCategory);
 		Product product3=new Product(25.0,date,"Biscuit Major",250,15,800,"Available","Food",18,null,null,store,subCategory);
 		
-	/*	userServicesRemote.create(user);
-		userServicesRemote.create(user1);
+		userServicesRemote.create(admin);
+		userServicesRemote.create(c1);
+		userServicesRemote.create(c2);
+		userServicesRemote.create(s1);
+		userServicesRemote.create(s2);
 		
 		categoryServicesRemote.addCategory(category);
 		categoryServicesRemote.addCategory(category1);
@@ -80,5 +89,5 @@ public class DBpopulator {
 		*/
 		
 	}
-	
+
 }
